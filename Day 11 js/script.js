ecfbe57afcbb5f2440531d0f84f268110d86296f
ista0x1 <ismailista16@gmail.com>
2023-08-23 17:04:35 +0200

    first commit

---
+++ Day 11 js/script.js
@@ -0,0 +1,40 @@
+let age =21;
+let isStudent= true;
+let favoriteColors =['blue','red','green'];
+//Task 2
+let string = "''";
+console.log(string);
+// Task3
+let p = 'ista my age is '+age+'and im i a student '+isStudent+'';
+console.log(p);
+// Task5
+let animals = ['lion','cat', 'dog'];
+let animal='lion';
+let color = 'blue';
+if (animal in animal && color in favoriteColors) {
+    console.log('your favorite animal and color is '+animal+' '+color+'')
+}
+//task 6
+let number = alert('enter number');
+if (number > 0) {
+    console.log('number is positive');
+}
+else if(number == 0){
+    console.log('number is equal to zero')
+}
+else{
+    console.log("number is negative") ;   
+}
+//task 8
+let value = alert('enter value and i will check it if its true or false ')
+if(value === true){
+    let result ='true'
+    console.log(result);
+}
+else{
+    console.log(false)
+}
+//task 10
+//done
+//task 12
+//done
